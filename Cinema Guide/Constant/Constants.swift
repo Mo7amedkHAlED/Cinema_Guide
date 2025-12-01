5ffd7cf231e524ba1394b751cd0706aead887241
Mo7amedkHAlED <mohammdk393@gmail.com>
2025-12-01 22:33:49 +0200

    Add secure access for API_KEY and ACCESS_KEY using Build Settings

--- Cinema Guide/Constant/Constants.swift
+++ Cinema Guide/Constant/Constants.swift
@@ -9,8 +9,7 @@ import Foundation
 
 enum Constants {
     static let baseUrl: String = "https://api.themoviedb.org/3"
-    static let apiKey: String = "4161586266f205b871d0a94f21ea1ab7"
     static let imageBaseURL = "https://image.tmdb.org/t/p/w500"
-    static let accessKey: String =
-    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0MTYxNTg2MjY2ZjIwNWI4NzFkMGE5NGYyMWVhMWFiNyIsIm5iZiI6MTc2NDQ0NjI1Ny41ODQsInN1YiI6IjY5MmI1MDMxM2Q1NWQzZjU3MmI4ZjFhMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._KEoP3XlzIy2IMvEDRv7TNH5MtyvjQPZuiJwCV80s6g"
+    static let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String ?? ""
+    static let accessKey = Bundle.main.object(forInfoDictionaryKey: "ACCESS_KEY") as? String ?? ""
 }
